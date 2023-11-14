076c3e2df164143e2faef3f27a1f129111b355e6
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-11-13 21:13:24 -0500

    update

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const usersRoutes = require("./routes/users");
 const invoicesRoutes = require("./routes/invoices");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-  
+const cors = require('cors');
 const app = express();
-
+// Enable CORS for all routes
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cookieParser());
