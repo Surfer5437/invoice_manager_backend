9ba8d58885e4099e3ebbeb29d024199d4ea74498
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-11-13 19:14:58 -0500

    update

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ let db;
 if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: getDatabaseUri(),
-    ssl: false
+    ssl: true
   });
 } else {
   db = new Client(
