6037bca82648e6a9986dd876b21973e2fb68a615
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-11-15 05:30:06 -0500

    update

--- routes/auth.js
+++ routes/auth.js
@@ -21,6 +21,7 @@ const { BadRequestError } = require("../expressError");
  */
 
 router.post("/token", async function (req, res, next) {
+  console.log(req)
   try {
     const validator = jsonschema.validate(req.body, userAuthSchema);
     if (!validator.valid) {
